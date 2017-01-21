72d7477ce525d2664082bbbf82894a39a6650add
2003cah <2003cah@gmail.com>
2017-01-20 19:16:18 -0500

    woahdude

--- code.rb
+++ code.rb
@@ -2,21 +2,28 @@ require 'discordrb'
 require 'configatron'
 require_relative 'config.rb'
 
-bot = Discordrb::Commands::CommandBot.new token: configatron.token, type: :user, prefix: 'cah!', advanced_functionality: false, help_command: false, parse_self: true, help_available: false, debug: true #, log_mode: :quiet
+bot = Discordrb::Commands::CommandBot.new token: configatron.token, type: :user, prefix: 'cah!', advanced_functionality: false, help_command: false, parse_self: true, help_available: false, debug: true, log_mode: :quiet
 
-bot.set_user_permission(271076191161548800, 1)
+bot.set_user_permission(228290433057292288, 1)
 
 bot.command(:eval, help_available: false, permission_message: false, permission_level: 1) do |event, *code|
   begin
     event.message.delete
-    m = (eval code.join(' '))
-    sleep 5
-    m.delete
+    eval code.join(' ')
   rescue => e
     event << "Ah geez, something went wrong, it says:"
     event << "```"
     event << "#{e} ```"
   end
+end
+
+bot.command(:ping, help_available: false, max_args: 0, permission_message: false, permission_level: 1) do |event|
+  m = event.respond('Pinging!')
+  m.edit "Pong! Hey, that took #{((Time.now - event.timestamp) * 1000).to_i}ms."
+end
+
+bot.command([:servercount, :servcount], help_available: false, max_args: 0, permission_message: false, permission_level: 1) do |event|
+  event.respond "Cah, you're in **#{bot.servers.count}** servers right now"
 end
 
 bot.command(:say, help_available: false, permission_message: false, permission_level: 1, min_args: 0) do |event|
