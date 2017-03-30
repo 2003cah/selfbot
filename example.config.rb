2ff86fc791b05612d37a36142ca0ca7ba93067e2
2003cah <2003cah@gmail.com>
2017-03-29 20:41:28 -0400

    how did I not commit this yet

--- example.config.rb
+++ example.config.rb
@@ -1,1 +1,4 @@
-configatron.token = 'YOUR_TOKEN'
+configatron.token = 'mfa.YoUr-T_oKen.HEre'
+configatron.prefix = ' Your Prefered Prefix'
+configatron.name = 'Your username, or whatever you want the selfbot to call you'
+configatron.id = 'In order for the selfbot to properly work, place your User ID here'
